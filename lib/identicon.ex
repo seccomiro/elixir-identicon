d27e0a2a43fab2d9e05e40df77f6f753d66e619a
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-02-16 13:11:42 -0300

    Generate pixel map

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,6 +5,7 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
+    |> build_pixel_map
   end
 
   def hash_input(input) do
@@ -39,5 +40,18 @@ defmodule Identicon do
       rem(code, 2) == 0
     end
     %Identicon.Image{image | grid: grid}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map =
+      grid
+      |> Enum.map fn({_code, index}) ->
+        horizontal = rem(index, 5) * 50
+        vertical = div(index, 5) * 5
+        top_left = {horizontal, vertical}
+        bottom_right = {horizontal + 50, vertical + 50}
+        {top_left, bottom_right}
+      end
+      %Identicon.Image{image | pixel_map: pixel_map}
   end
 end
