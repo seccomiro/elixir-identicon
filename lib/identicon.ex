14517c79f69d506fa1b9126927cf5aa84bcd0053
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-02-16 13:40:36 -0300

    Execute mix format on whole project

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -27,8 +27,9 @@ defmodule Identicon do
       hex
       |> Enum.chunk(3)
       |> Enum.map(&mirror_row/1)
-      |> List.flatten
-      |> Enum.with_index
+      |> List.flatten()
+      |> Enum.with_index()
+
     %Identicon.Image{image | grid: grid}
   end
 
@@ -38,32 +39,37 @@ defmodule Identicon do
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
-    grid = Enum.filter grid, fn({code, _index}) ->
-      rem(code, 2) == 0
-    end
+    grid =
+      Enum.filter(grid, fn {code, _index} ->
+        rem(code, 2) == 0
+      end)
+
     %Identicon.Image{image | grid: grid}
   end
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
     pixel_map =
       grid
-      |> Enum.map fn({_code, index}) ->
+      |> Enum.map(fn {_code, index} ->
         horizontal = rem(index, 5) * 50
         vertical = div(index, 5) * 50
         top_left = {horizontal, vertical}
         bottom_right = {horizontal + 50, vertical + 50}
         {top_left, bottom_right}
-      end
-      %Identicon.Image{image | pixel_map: pixel_map}
+      end)
+
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 
   def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
     image = :egd.create(250, 250)
     fill = :egd.color(color)
+
     pixel_map
-    |> Enum.each fn({start, stop}) ->
+    |> Enum.each(fn {start, stop} ->
       :egd.filledRectangle(image, start, stop, fill)
-    end
+    end)
+
     :egd.render(image)
   end
 
